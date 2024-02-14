52e3bbf3bf56eb0bea74a4cd95fed9599622351b
Tristan Phipson <tristanphip26@gmail.com>
2024-02-14 18:13:10 +0200

    feature/mock_data: added mock data SQL files to be inserted. Note insert_order.txt.

---
+++ mock_data/difficulty.sql
@@ -0,0 +1,4 @@
+insert into difficulty (difficulty_id, difficulty_description, years_experience) values (1, 'Beginner', 0),
+(2, 'Intermediate', 2),
+(3, 'Advanced', 5),
+(4, 'Expert', 8);
---
+++ mock_data/programs.sql
@@ -0,0 +1,4 @@
+insert into programs (program_name, difficulty_id, program_description) values ('Weight Loss Challenge', 1, 'This program focuses on high-intensity interval training (HIIT) and circuit training to help participants lose weight and improve cardiovascular health.'),
+('Muscle Gain Program', 2, 'Designed for individuals looking to build muscle mass, this program incorporates strength training exercises targeting major muscle groups.'),
+('Core Strength Bootcamp', 3, 'A bootcamp-style program aimed at improving core strength and stability through a variety of bodyweight exercises and functional movements.'),
+('Endurance Training Plan', 4, 'This program is geared towards improving endurance and stamina through long-distance running, cycling, and other cardiovascular exercises.');
