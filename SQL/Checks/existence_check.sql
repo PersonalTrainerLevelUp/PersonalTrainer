386506b78a079dca4d4d3ea5186d0209a6677ba4
tristanphips <tristan.phipson@bbd.co.za>
2024-02-15 15:15:56 +0200

    added all tables to existence check

--- SQL/Checks/existence_check.sql
+++ SQL/Checks/existence_check.sql
@@ -5,3 +5,59 @@ SELECT EXISTS (
         schemaname = 'public' AND 
         tablename  = 'clients'
     );
+    
+    SELECT EXISTS (
+    SELECT FROM 
+        pg_tables
+    WHERE 
+        schemaname = 'public' AND 
+        tablename  = 'billing'
+    );
+    
+    SELECT EXISTS (
+    SELECT FROM 
+        pg_tables
+    WHERE 
+        schemaname = 'public' AND 
+        tablename  = 'client_program'
+    );
+    
+    SELECT EXISTS (
+    SELECT FROM 
+        pg_tables
+    WHERE 
+        schemaname = 'public' AND 
+        tablename  = 'difficulty'
+    );
+    
+    SELECT EXISTS (
+    SELECT FROM 
+        pg_tables
+    WHERE 
+        schemaname = 'public' AND 
+        tablename  = 'exercises'
+    );
+    
+    SELECT EXISTS (
+    SELECT FROM 
+        pg_tables
+    WHERE 
+        schemaname = 'public' AND 
+        tablename  = 'payments'
+    );
+    
+    SELECT EXISTS (
+    SELECT FROM 
+        pg_tables
+    WHERE 
+        schemaname = 'public' AND 
+        tablename  = 'program_exercise'
+    );
+    
+    SELECT EXISTS (
+    SELECT FROM 
+        pg_tables
+    WHERE 
+        schemaname = 'public' AND 
+        tablename  = 'programs'
+    );
